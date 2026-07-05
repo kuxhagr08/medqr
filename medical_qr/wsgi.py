38260ce060b8ba73ba485b387eeac0c3aeff0ec8
Kushagra shrivastava <shrivastavakushagra22@gmail.com>
2026-07-05 14:05:18 +0530

    Auto-run migrations on Vercel boot

--- medical_qr/wsgi.py
+++ medical_qr/wsgi.py
@@ -8,9 +8,13 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+from django.core.management import call_command
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'medical_qr.settings')
 
 application = get_wsgi_application()
+
+# If running on Vercel serverless, run migrations on boot since /tmp is ephemeral
+if os.environ.get('VERCEL') == '1':
+    call_command('migrate', interactive=False)
